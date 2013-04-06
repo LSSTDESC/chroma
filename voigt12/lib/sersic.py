80b35653d78d8fd0c84a829ddd3a39f3bb323e38
jmeyers314 <jmeyers314@gmail.com>
2013-04-06 10:11:42 -0700

    =accounted for degeneracy between a,b,phi when initializing with C11, C12, C22

--- voigt12/lib/sersic.py
+++ voigt12/lib/sersic.py
@@ -54,8 +54,11 @@ class Sersic(object):
             self.C22 = C22
             # want to keep some bookkeepping parameters around though...
             one_over_a_squared = 0.5 * (C11 + C22 + np.sqrt((C11 - C22)**2 + 4 * C12**2))
-            self.a = np.sqrt(1. / one_over_a_squared)
             one_over_b_squared = C11 + C22 - one_over_a_squared
+            #there's degeneracy between a, b and phi at this point so enforce a > b
+            if one_over_a_squared > one_over_b_squared:
+                one_over_a_squared, one_over_b_squared = one_over_b_squared, one_over_a_squared
+            self.a = np.sqrt(1. / one_over_a_squared)
             self.b = np.sqrt(1. / one_over_b_squared)
             self.r_e = np.sqrt(self.a * self.b)
             twophi = np.arctan2(2 * C12 / (one_over_a_squared - one_over_b_squared),
@@ -159,8 +162,8 @@ if __name__ == '__main__':
 
     # a, b, phi
     phi = 0.1
-    a = 0.2
-    b = 0.5
+    a = 0.5
+    b = 0.2
     s1 = Sersic(y0, x0, n, peak=peak, a=a, b=b, phi=phi)
     print 'It is an error if following 9 or so values do not all match'
     print s1(0.2, 2.1)
@@ -180,30 +183,30 @@ if __name__ == '__main__':
 
     # r_e, emag, phi
     emag = (a**2.0 - b**2.0) / (a**2.0 + b**2.0)
-    s5 = Sersic(y0, x0, n, peak=peak, r_e=r_e, emag=emag, phi=phi)
-    print s5(0.2, 2.1)
+    s4 = Sersic(y0, x0, n, peak=peak, r_e=r_e, emag=emag, phi=phi)
+    print s4(0.2, 2.1)
 
     # r_e, gmag, phi
     gmag = (a - b) / (a + b)
-    s6 = Sersic(y0, x0, n, peak=peak, r_e=r_e, gmag=gmag, phi=phi)
-    print s6(0.2, 2.1)
+    s5 = Sersic(y0, x0, n, peak=peak, r_e=r_e, gmag=gmag, phi=phi)
+    print s5(0.2, 2.1)
 
     # r_e, e1, e2
     e1 = emag * np.cos(2.0 * phi)
     e2 = emag * np.sin(2.0 * phi)
-    s7 = Sersic(y0, x0, n, peak=peak, r_e=r_e, e1=e1, e2=e2)
-    print s7(0.2, 2.1)
+    s6 = Sersic(y0, x0, n, peak=peak, r_e=r_e, e1=e1, e2=e2)
+    print s6(0.2, 2.1)
 
     # r_e, g1, g2
     g1 = gmag * np.cos(2.0 * phi)
     g2 = gmag * np.sin(2.0 * phi)
-    s8 = Sersic(y0, x0, n, peak=peak, r_e=r_e, g1=g1, g2=g2)
-    print s8(0.2, 2.1)
+    s7 = Sersic(y0, x0, n, peak=peak, r_e=r_e, g1=g1, g2=g2)
+    print s7(0.2, 2.1)
 
     # FWHM instead of r_e
     FWHM = 2 * r_e * (np.log(2.0) / Sersic.compute_kappa(n))**n
-    s4 = Sersic(y0, x0, n, peak=peak, r_e=r_e, b_over_a=b_over_a, phi=phi)
-    print s4(0.2, 2.1)
+    s8 = Sersic(y0, x0, n, peak=peak, r_e=r_e, b_over_a=b_over_a, phi=phi)
+    print s8(0.2, 2.1)
 
     # flux instead of peak
     flux=Sersic.compute_flux(n, r_e, peak)
@@ -221,3 +224,78 @@ if __name__ == '__main__':
     print s7.r_e
     print s8.r_e
     print s9.r_e
+
+
+    # make sure have access to a
+    print 'It is an error if following 9 or so values do not all match'
+    print s1.a
+    print s2.a
+    print s3.a
+    print s4.a
+    print s5.a
+    print s6.a
+    print s7.a
+    print s8.a
+    print s9.a
+
+
+    # make sure have access to b
+    print 'It is an error if following 9 or so values do not all match'
+    print s1.b
+    print s2.b
+    print s3.b
+    print s4.b
+    print s5.b
+    print s6.b
+    print s7.b
+    print s8.b
+    print s9.b
+
+
+    # make sure have access to phi
+    print 'It is an error if following 9 or so values do not all match'
+    print s1.phi
+    print s2.phi
+    print s3.phi
+    print s4.phi
+    print s5.phi
+    print s6.phi
+    print s7.phi
+    print s8.phi
+    print s9.phi
+
+    # make sure have access to C11
+    print 'It is an error if following 9 or so values do not all match'
+    print s1.C11
+    print s2.C11
+    print s3.C11
+    print s4.C11
+    print s5.C11
+    print s6.C11
+    print s7.C11
+    print s8.C11
+    print s9.C11
+
+    # make sure have access to C22
+    print 'It is an error if following 9 or so values do not all match'
+    print s1.C22
+    print s2.C22
+    print s3.C22
+    print s4.C22
+    print s5.C22
+    print s6.C22
+    print s7.C22
+    print s8.C22
+    print s9.C22
+
+    # make sure have access to C12
+    print 'It is an error if following 9 or so values do not all match'
+    print s1.C12
+    print s2.C12
+    print s3.C12
+    print s4.C12
+    print s5.C12
+    print s6.C12
+    print s7.C12
+    print s8.C12
+    print s9.C12
