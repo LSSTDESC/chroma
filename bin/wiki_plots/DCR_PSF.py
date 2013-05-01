9a0396c2b760316714266d6dbfdbdc47259d4ce4
jmeyers314 <jmeyers314@gmail.com>
2013-05-01 14:36:52 -0700

    =automatically create animated gif

--- bin/wiki_plots/DCR_PSF.py
+++ bin/wiki_plots/DCR_PSF.py
@@ -1,5 +1,8 @@
 # Make some photon histograms against wavelength and against refraction
 
+import os
+import subprocess
+
 import numpy
 import matplotlib.pyplot as plt
 
@@ -17,15 +20,20 @@ def moffat1d(fwhm, beta, center=0.0):
 def photon_hists(wave, photons, title, outfile):
     cwave = numpy.linspace(500, 860, 256)
 
-    fig, (ax1, ax2) = plt.subplots(2, figsize=(10,5), dpi=80)
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(10,6.5), dpi=80)
     ax1.set_title(title)
     w = numpy.logical_and(wave > 500, wave < 850)
     scale = 1.2 * photons[w].max()
     ax1.plot(wave, photons/scale, color='black')
     ax1.set_xlim(500, 850)
     ax1.set_ylim(0.0, 1.0)
+    ax1.set_xlabel('Wavelength (nm)')
+    ax1.set_ylabel('photons/s/cm$^2$/$\AA$')
     ax2.set_xlim(21.6, 22.6)
     ax2.set_ylim(0.0, 1.0)
+    ax2.set_xlabel('Refraction (arcsec)')
+    ax2.set_ylabel('photons/s/cm$^2$/arcsec')
+
 
     filters = ['LSST_r', 'LSST_i']
     for f in filters:
@@ -54,7 +62,7 @@ def photon_hists(wave, photons, title, outfile):
         ax2.plot(R[w], angle_dens[w]/angle_dens[w].max() / 1.2, color='black')
         chroma.chroma_fill_plot(R[w], angle_dens[w]/angle_dens[w].max() / 1.2, color[w], axes=ax2)
 
-    FWHM = 0.6
+    FWHM = 0.6 # arcseconds
     beta = 2.619 # measured from phoSim?
     moffat = moffat1d(FWHM, beta, 22.1)
     xs = numpy.linspace(21.6, 22.6, 100)
@@ -71,10 +79,10 @@ def main():
         photons = sflux * swave
         photon_hists(swave, photons,
                      '{} star, zenith = 30 degrees'.format(starname),
-                     'output/DCR_PSF/DCR_PSF.{}.png'.format(starname))
+                     'output/d.{}.png'.format(starname))
 
-    gals = ['CWW_E', 'CWW_Im', 'CWW_Sbc']
-    galnames = ['E', 'Im', 'Sbc']
+    gals = ['CWW_E', 'CWW_Sbc', 'CWW_Im']
+    galnames = ['E', 'Sbc', 'Im']
     for gal, galname in zip(gals, galnames):
         gal_file = '../../data/SEDs/{}_ext.ascii'.format(gal)
         gdata = numpy.genfromtxt(gal_file)
@@ -83,8 +91,16 @@ def main():
         gwave *= 2.0 # z = 1.0
         photon_hists(gwave, photons,
                      '{} galaxy, z = 1.0, zenith = 30 degrees'.format(galname),
-                     'output/DCR_PSF/DCR_PSF.{}.png'.format(galname))
+                     'output/d.{}.png'.format(galname))
 
+    cmd = 'convert -delay 80 -loop 0 '
+    for name in starnames+galnames:
+        cmd += 'output/d.{}.png '.format(name)
+    cmd += 'output/DCR_PSF_anim.gif'
+    subprocess.call(cmd, shell=True)
+
+    for name in starnames+galnames:
+        os.remove('output/d.{}.png'.format(name))
 
 if __name__ == '__main__':
     main()
