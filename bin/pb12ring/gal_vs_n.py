d4ae2e6cb364b94e1626e8877d9bf685e1f13e43
jmeyers314 <jmeyers314@gmail.com>
2013-06-11 16:52:19 -0700

    =cartoon for seeing different Sersic n but holding r2 constant

---
+++ bin/pb12ring/gal_vs_n.py
@@ -0,0 +1,53 @@
+import numpy
+import lmfit
+
+import _mypath
+import chroma
+
+def fiducial_galaxy():
+    gparam = lmfit.Parameters()
+    gparam.add('x0', value=0.0)
+    gparam.add('y0', value=0.0)
+    gparam.add('n', value=0.5, vary=False)
+    gparam.add('r_e', value=1.0)
+    gparam.add('flux', value=1.0, vary=False)
+    gparam.add('gmag', value=0.0)
+    gparam.add('phi', value=0.0)
+    return gparam
+
+gparam1 = fiducial_galaxy()
+gparam1['n'].value = 0.5
+
+s_engine = chroma.imgen.GalSimSEngine(size=15, oversample_factor=7)
+gal = chroma.gal_model.SGal(gparam1, s_engine)
+gal.set_uncvl_r2((0.27/0.2)**2)
+im05 = s_engine.get_uncvl_image(gal.gparam0, pixsize=1./7)
+
+gparam1['n'].value = 1.0
+gal = chroma.gal_model.SGal(gparam1, s_engine)
+gal.set_uncvl_r2((0.27/0.2)**2)
+im10 = s_engine.get_uncvl_image(gal.gparam0, pixsize=1./7)
+
+gparam1['n'].value = 4.0
+gal = chroma.gal_model.SGal(gparam1, s_engine)
+gal.set_uncvl_r2((0.27/0.2)**2)
+im40 = s_engine.get_uncvl_image(gal.gparam0, pixsize=1./7)
+
+import matplotlib.pyplot as plt
+f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
+vmin = im10.min()
+vmax = im10.max()
+
+ax1.imshow(im05, vmin=vmin, vmax=vmax)
+ax2.imshow(im10, vmin=vmin, vmax=vmax)
+ax3.imshow(im40, vmin=vmin, vmax=vmax)
+ax4.plot(im05.sum(axis=0))
+ax5.plot(im10.sum(axis=0))
+ax6.plot(im40.sum(axis=0))
+ax4.set_ylim(bottom=0)
+ax5.set_ylim(bottom=0)
+ax6.set_ylim(bottom=0)
+ax4.set_xlim([0,15*7])
+ax5.set_xlim([0,15*7])
+ax6.set_xlim([0,15*7])
+plt.show()
