b66edd5d683716f9ae2297d4114593a2560e9f44
jmeyers314 <jmeyers314@gmail.com>
2013-06-13 15:08:26 -0700

    =use same air_refractive_index formula as used by phosim_3.2.1

--- chroma/atmdisp.py
+++ chroma/atmdisp.py
@@ -4,6 +4,35 @@ import scipy.integrate
 def air_refractive_index(wave, pressure=69.328, temperature=293.15, H2O_pressure=1.067):
     '''Return the refractive index of air as function of wavelength.
 
+    Uses the formulae given in Filippenko (1982), which appears to come from Edlen (1953),
+    and Coleman, Bozman, and Meggers (1960).  The units of the original formula are non-SI,
+    being mmHg for pressure (and water vapor pressure), and degrees C for temperature.  This
+    function accepts SI units however, and transforms them when plugging in the formula.
+    Default values for `pressure`, `temperature`, and `H2O_pressure` are taken from LSST PhoSim
+    defaults.
+
+    Arguments
+    ---------
+    wave -- wavelength in nanometers
+    pressure -- in kiloPascals (default 69.328 kPa = 520 mmHg)
+    temperature -- in Kelvin (default 293.15 kPa = 20 C)
+    H2O_pressure -- in kiloPascals (default 1.067 kPa = 8 mmHg)
+    '''
+
+    sigma_squared = 1.0 / (wave * 1.e-3)**2.0 # inverse wavenumber squared in um^-2
+
+    n_minus_one = (64.328 + (29498.1 / (146.0 - sigma_squared))
+                   + (255.4 / (41.0 - sigma_squared))) * 1.e-6
+    P = pressure * 7.50061683 # kPa -> mmHg
+    T = temperature - 273.15 # K -> C
+    W = H2O_pressure * 7.50061683 # kPa -> mmHg
+    n_minus_one *= P * (1.0 + (1.049 - 0.0157 * T) * 1.e-6 * P) / (720.883 * (1.0 + 0.003661 * T))
+    n_minus_one -= (0.0624 - 0.000680 * sigma_squared)/(1.0 + 0.003661 * T) * W * 1.e-6
+    return n_minus_one + 1.0
+
+def air_refractive_index2(wave, pressure=69.328, temperature=293.15, H2O_pressure=1.067):
+    '''Return the refractive index of air as function of wavelength.
+
     Uses the formulae given by Allens Astrophysical Quantities (Cox et al. 2001), including effects
     due to `pressure`, `temperature`, and the partial pressure of water vapor: `H2O_pressure`.
     Default values for `pressure`, `temperature`, and `H2O_pressure` are taken from LSST PhoSim
