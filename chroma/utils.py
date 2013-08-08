cb9ba2ba97bc905d5b3b1b725cdc7f4c476a6cf7
jmeyers314 <jmeyers314@gmail.com>
2013-08-08 11:54:36 -0700

    =refactor ring test and start to add file output option

--- chroma/utils.py
+++ chroma/utils.py
@@ -36,40 +36,80 @@ def ringtest(gamma, n_ring, gen_target_image, gen_init_param, measure_ellip, sil
     betas = numpy.linspace(0.0, 2.0 * numpy.pi, n_ring, endpoint=False)
     ellip0s = []
     ellip180s = []
-    if not silent:
-        with astropy.utils.console.ProgressBar(2 * n_ring) as bar:
-            for beta in betas:
-                #measure ellipticity at beta along the ring
-                target_image0 = gen_target_image(gamma, beta)
-                init_param0 = gen_init_param(gamma, beta)
-                ellip0 = measure_ellip(target_image0, init_param0)
-                ellip0s.append(ellip0)
-                bar.update()
 
-                #repeat with beta on opposite side of the ring (i.e. +180 deg)
-                target_image180 = gen_target_image(gamma, beta + numpy.pi)
-                init_param180 = gen_init_param(gamma, beta + numpy.pi)
-                ellip180 = measure_ellip(target_image180, init_param180)
-                ellip180s.append(ellip180)
+    def work():
+        #measure ellipticity at beta along the ring
+        target_image0 = gen_target_image(gamma, beta)
+        init_param0 = gen_init_param(gamma, beta)
+        ellip0 = measure_ellip(target_image0, init_param0)
+        ellip0s.append(ellip0)
+
+        #repeat with beta on opposite side of the ring (i.e. +180 deg)
+        target_image180 = gen_target_image(gamma, beta + numpy.pi)
+        init_param180 = gen_init_param(gamma, beta + numpy.pi)
+        ellip180 = measure_ellip(target_image180, init_param180)
+        ellip180s.append(ellip180)
+
+    if not silent:
+        with astropy.utils.console.ProgressBar(n_ring) as bar:
+            for beta in betas:
+                work()
                 bar.update()
-                gamma_hats = [0.5 * (e0 + e1) for e0, e1 in zip(ellip0s, ellip180s)]
     else:
         for beta in betas:
-            #measure ellipticity at beta along the ring
-            target_image0 = gen_target_image(gamma, beta)
-            init_param0 = gen_init_param(gamma, beta)
-            ellip0 = measure_ellip(target_image0, init_param0)
-            ellip0s.append(ellip0)
+            work()
 
-            #repeat with beta on opposite side of the ring (i.e. +180 deg)
-            target_image180 = gen_target_image(gamma, beta + numpy.pi)
-            init_param180 = gen_init_param(gamma, beta + numpy.pi)
-            ellip180 = measure_ellip(target_image180, init_param180)
-            ellip180s.append(ellip180)
-            gamma_hats = [0.5 * (e0 + e1) for e0, e1 in zip(ellip0s, ellip180s)]
-
+    gamma_hats = [0.5 * (e0 + e1) for e0, e1 in zip(ellip0s, ellip180s)]
     gamma_hat = numpy.mean(gamma_hats)
     return gamma_hat
+
+def ringtest_with_output(gamma, n_ring, gen_target_image, gen_init_param, measure_ellip,
+                         outfile, silent=False):
+    ''' Performs a shear calibration ringtest, and outputs intermediate results to a FITS file.
+
+    Produces "true" images uniformly spread along a ring in ellipticity space using the supplied
+    `gen_target_image` function.  Then tries to fit these images, (returning ellipticity estimates)
+    using the supplied `measure_ellip` function with the fit initialized by the supplied
+    `gen_init_param` function.
+
+    The "true" images are sheared by `gamma` (handled by passing through to `gen_target_image`).
+    Images are generated in pairs separated by 180 degrees on the ellipticity plane to minimize shape
+    noise.
+
+    Ultimately returns an estimate of the applied shear (`gamma_hat`), which can then be compared to
+    the input shear `gamma` in an external function to estimate shear calibration parameters.
+    '''
+
+    betas = numpy.linspace(0.0, 2.0 * numpy.pi, n_ring, endpoint=False)
+    ellip0s = []
+    ellip180s = []
+
+    def work():
+        #measure ellipticity at beta along the ring
+        target_image0 = gen_target_image(gamma, beta)
+        init_param0 = gen_init_param(gamma, beta)
+        ellip0 = measure_ellip(target_image0, init_param0)
+        ellip0s.append(ellip0)
+
+        #repeat with beta on opposite side of the ring (i.e. +180 deg)
+        target_image180 = gen_target_image(gamma, beta + numpy.pi)
+        init_param180 = gen_init_param(gamma, beta + numpy.pi)
+        ellip180 = measure_ellip(target_image180, init_param180)
+        ellip180s.append(ellip180)
+
+    if not silent:
+        with astropy.utils.console.ProgressBar(n_ring) as bar:
+            for beta in betas:
+                work()
+                bar.update()
+    else:
+        for beta in betas:
+            work()
+
+    gamma_hats = [0.5 * (e0 + e1) for e0, e1 in zip(ellip0s, ellip180s)]
+    gamma_hat = numpy.mean(gamma_hats)
+    return gamma_hat
+
 
 def FWHM(data, pixsize=1.0):
     '''Compute the full-width at half maximum of a symmetric 2D distribution.  Assumes that measuring
