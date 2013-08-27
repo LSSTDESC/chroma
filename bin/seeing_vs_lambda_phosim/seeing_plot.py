5cc9bb60eba5bc27ff657e89a1093bc5df4363fa
jmeyers314 <jmeyers314@gmail.com>
2013-08-27 14:24:21 -0700

    =zoom into r+i band for FWHM vs lambda

--- bin/seeing_vs_lambda_phosim/seeing_plot.py
+++ bin/seeing_vs_lambda_phosim/seeing_plot.py
@@ -1,11 +1,8 @@
-import sys, os
+import os
 import pickle
 
 import numpy
-import lmfit
 import matplotlib.pyplot as plt
-from astropy import wcs
-from astropy.io import fits
 
 waves = {'u': numpy.arange(325, 401, 25),
          'g': numpy.arange(400, 551, 25),
@@ -20,11 +17,14 @@ colors = {'u':'violet',
           'z':'red',
           'Y':'black'}
 
-nfiles = sum(map(len, waves.values())) * 2
-
 values = pickle.load(open('seeing_vs_wave.pik'))
 
-# fwhm plot
+if not os.path.isdir('plots'):
+    os.mkdir('plots')
+
+
+# fwhm plot with all filters and both with and without instrument
+print 'Plotting FWHM for all filters both with and without instrument'
 fwhm_fig = plt.figure()
 fwhm_ax = plt.subplot(111)
 fwhm_ax.set_xlabel('wavelength (nm)')
@@ -47,8 +47,11 @@ w600 = numpy.where(numpy.logical_and(values['wave'] == 600, values['mode'] == 2)
 y600 = values[w600]['fwhm_x']
 y= y600 * (x/600.)**(-0.2)
 fwhm_ax.plot(x, y)
+fwhm_fig.savefig('plots/FWHM.pdf')
 
-# beta plot
+
+# beta plot with all filters and both with and without instrument
+print 'Plotting beta for all filters both with and without instrument'
 beta_fig = plt.figure()
 beta_ax = plt.subplot(111)
 for k in waves.keys():
@@ -60,9 +63,76 @@ for k in waves.keys():
                      ls='none', marker='o', color=colors[k], alpha=0.2)
 beta_ax.set_xlabel('wavelength (nm)')
 beta_ax.set_ylabel(r'$\beta$')
-
-if not os.path.isdir('plots'):
-    os.mkdir('plots')
-
-fwhm_fig.savefig('plots/FWHM.pdf')
 beta_fig.savefig('plots/beta.pdf')
+
+# changing colors for r+i only plots:
+colors['r'] = 'blue'
+colors['i'] = 'red'
+
+# fwhm plot with only r+i bands and atmosphere only
+print 'Plotting FWHM for r+i for atmosphere only'
+fwhm_fig = plt.figure()
+fwhm_ax = plt.subplot(111)
+fwhm_ax.set_xlabel('wavelength (nm)')
+fwhm_ax.set_ylabel('FWHM (pixels)')
+fwhm_ax.set_ylim(2.8, 3.6)
+fwhm_ax.set_xlim(500, 900)
+for k in ['r', 'i']:
+    ind = numpy.logical_and(values['filter'] == k, values['mode'] == 2)
+    fwhm_ax.errorbar(values[ind]['wave'], values[ind]['fwhm_x'], values[ind]['fwhm_x_err'],
+                     ls='none', marker='o', color=colors[k])
+
+x = numpy.linspace(300, 1100, 100)
+w700 = numpy.where(numpy.logical_and(values['wave'] == 700, values['mode'] == 2))[0][0]
+y700 = values[w700]['fwhm_x']
+y= y700 * (x/700.)**(-0.2)
+fwhm_ax.plot(x, y, color='k')
+# also plot filters in background
+rwave, rthru = numpy.genfromtxt('../../data/filters/LSST_r.dat').T
+rthru /= rthru.max()
+rthru *= 0.3
+rthru += 2.8
+fwhm_ax.fill_between(rwave, rthru, color='blue', alpha=0.1)
+iwave, ithru = numpy.genfromtxt('../../data/filters/LSST_i.dat').T
+ithru /= ithru.max()
+ithru *= 0.3
+ithru += 2.8
+fwhm_ax.fill_between(iwave, ithru, color='red', alpha=0.1)
+# save
+fwhm_fig.savefig('plots/FWHM.r+i.atmos.pdf')
+
+
+# fwhm plot with only r+i bands and atmosphere only
+print 'Plotting FWHM for r+i for atmosphere and instrument'
+fwhm_fig = plt.figure()
+fwhm_ax = plt.subplot(111)
+fwhm_ax.set_xlabel('wavelength (nm)')
+fwhm_ax.set_ylabel('FWHM (pixels)')
+fwhm_ax.set_ylim(2.8, 3.6)
+fwhm_ax.set_xlim(500, 900)
+for k in ['r', 'i']:
+    ind = numpy.logical_and(values['filter'] == k, values['mode'] == 1)
+    fwhm_ax.errorbar(values[ind]['wave'], values[ind]['fwhm_x'], values[ind]['fwhm_x_err'],
+                     ls='none', marker='o', color=colors[k], alpha=0.2)
+    ind = numpy.logical_and(values['filter'] == k, values['mode'] == 2)
+    fwhm_ax.errorbar(values[ind]['wave'], values[ind]['fwhm_x'], values[ind]['fwhm_x_err'],
+                     ls='none', marker='o', color=colors[k])
+
+x = numpy.linspace(300, 1100, 100)
+w700 = numpy.where(numpy.logical_and(values['wave'] == 700, values['mode'] == 2))[0][0]
+y700 = values[w700]['fwhm_x']
+y= y700 * (x/700.)**(-0.2)
+fwhm_ax.plot(x, y, color='k')
+# also plot filters in background
+rwave, rthru = numpy.genfromtxt('../../data/filters/LSST_r.dat').T
+rthru /= rthru.max()
+rthru *= 0.3
+rthru += 2.8
+fwhm_ax.fill_between(rwave, rthru, color='blue', alpha=0.1)
+iwave, ithru = numpy.genfromtxt('../../data/filters/LSST_i.dat').T
+ithru /= ithru.max()
+ithru *= 0.3
+ithru += 2.8
+fwhm_ax.fill_between(iwave, ithru, color='red', alpha=0.1)
+# save
+fwhm_fig.savefig('plots/FWHM.r+i.atmos+inst.pdf')
