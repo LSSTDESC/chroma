a4dfac4b484eab415710baf796c389a20308688e
jmeyers314 <jmeyers314@gmail.com>
2014-01-16 15:23:02 -0800

    rename chromatic seeing power law index alpha

--- chroma/seeing.py
+++ chroma/seeing.py
@@ -1,7 +1,7 @@
 import scipy.integrate
 
-def relative_second_moment_radius(wave, photons, n=-0.2):
+def relative_second_moment_radius(wave, photons, alpha=-0.2):
     ''' Returns the second moment radius of PSF with specified SED, normalized at 500nm.
     '''
-    return (scipy.integrate.simps(photons * (wave/500) ** (2*n), wave) /
+    return (scipy.integrate.simps(photons * (wave/500) ** (2*alpha), wave) /
             scipy.integrate.simps(photons, wave))
