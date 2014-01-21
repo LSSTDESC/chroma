5cc2bd703388357e474c3829b424dd3491086703
jmeyers314 <jmeyers314@gmail.com>
2014-01-21 13:31:23 -0800

    precompute analytic magnitude/DCR/seeing results

---
+++ bin/analytic/analytic_table.py
@@ -0,0 +1,135 @@
+import cPickle
+
+import numpy as np
+from numpy.lib.recfunctions import append_fields
+import astropy.utils.console as console
+
+import _mypath
+import chroma
+
+ugrizy =  [('LSST_u', np.float32),
+           ('LSST_g', np.float32),
+           ('LSST_r', np.float32),
+           ('LSST_i', np.float32),
+           ('LSST_z', np.float32),
+           ('LSST_y', np.float32)]
+E =       [('Euclid_150', np.float32),
+           ('Euclid_250', np.float32),
+           ('Euclid_350', np.float32),
+           ('Euclid_450', np.float32)]
+ugrizyE = [('LSST_u', np.float32),
+           ('LSST_g', np.float32),
+           ('LSST_r', np.float32),
+           ('LSST_i', np.float32),
+           ('LSST_z', np.float32),
+           ('LSST_y', np.float32),
+           ('Euclid_150', np.float32),
+           ('Euclid_250', np.float32),
+           ('Euclid_350', np.float32),
+           ('Euclid_450', np.float32)]
+
+def compute_mags_moments(sed, filters):
+    out = np.recarray((1,), dtype = [('mag', ugrizyE),
+                                        ('Rbar', ugrizy),
+                                        ('V', ugrizy),
+                                        ('S_m02', ugrizy),
+                                        ('S_p06', E),
+                                        ('S_p10', E)])
+    for filter_name, bandpass in filters.iteritems():
+        try:
+            out[0]['mag'][filter_name] = sed.magnitude(bandpass)
+        except ValueError:
+            out[0]['mag'][filter_name] = np.nan
+        if filter_name.startswith('Euclid'):
+            try:
+                out[0]['S_p06'][filter_name] = sed.seeing_shift(bandpass, alpha=0.6)
+                out[0]['S_p10'][filter_name] = sed.seeing_shift(bandpass, alpha=1.0)
+            except ValueError:
+                out[0]['S_p06'][filter_name] = np.nan
+                out[0]['S_p10'][filter_name] = np.nan
+        else:
+            try:
+                DCR_mom = sed.DCR_moment_shifts(bandpass, zenith=45.0)
+                out[0]['Rbar'][filter_name] = DCR_mom[0]
+                out[0]['V'][filter_name] = DCR_mom[1]
+                out[0]['S_m02'][filter_name] = sed.seeing_shift(bandpass, alpha=-0.2)
+            except ValueError:
+                out[0]['Rbar'][filter_name] = np.nan
+                out[0]['V'][filter_name] = np.nan
+                out[0]['S_m02'][filter_name] = np.nan
+    return out
+
+def construct_analytic_table():
+    spec_dir = '../../data/SEDs/'
+    filter_dir = '../../data/filters/'
+
+    # define SED types
+    star_types = ['uko5v',
+                  'ukb5iii',
+                  'uka5v',
+                  'ukf5v',
+                  'ukg5v',
+                  'ukk5v',
+                  'ukm5v']
+    gal_types= ['CWW_E_ext',
+                'KIN_Sa_ext',
+                'KIN_Sb_ext',
+                'CWW_Sbc_ext',
+                'CWW_Scd_ext',
+                'CWW_Im_ext',
+                'KIN_SB1_ext',
+                'KIN_SB6_ext']
+
+    # load filters
+    filter_names = ['LSST_u', 'LSST_g', 'LSST_r', 'LSST_i', 'LSST_z', 'LSST_y',
+                   'Euclid_150', 'Euclid_250', 'Euclid_350', 'Euclid_450']
+    filters = {}
+    for f in filter_names:
+        f_wave, f_throughput = np.genfromtxt(filter_dir + '{}.dat'.format(f)).T
+        filters[f] = chroma.Bandpass(f_wave, f_throughput)
+        filters[f].truncate(rel_throughput=0.01)
+
+    # start with stars
+    star_data = np.recarray((len(star_types),), dtype = [('star_type', 'a11'),
+                                                         ('mag', ugrizyE),
+                                                         ('Rbar', ugrizy),
+                                                         ('V', ugrizy),
+                                                         ('S_m02', ugrizy),
+                                                         ('S_p06', E),
+                                                         ('S_p10', E)])
+    for i, star_type in enumerate(star_types):
+        s_wave, s_flambda = np.genfromtxt(spec_dir + star_type + '.ascii').T
+        star_SED = chroma.SED(s_wave, s_flambda)
+        data = compute_mags_moments(star_SED, filters)
+        star_data[i]['star_type'] = star_type
+        for name in data.dtype.names:
+            star_data[i][name] = data[name]
+    np.save('stars.npy', star_data)
+
+    # now onto galaxies
+    gal_data = np.recarray((len(gal_types)*100,), dtype = [('gal_type', 'a11'),
+                                                           ('redshift', np.float32),
+                                                           ('mag', ugrizyE),
+                                                           ('Rbar', ugrizy),
+                                                           ('V', ugrizy),
+                                                           ('S_m02', ugrizy),
+                                                           ('S_p06', E),
+                                                           ('S_p10', E)])
+    i=0
+    with console.ProgressBar(100 * len(gal_types)) as bar:
+        for gal_type in gal_types:
+            g_wave, g_flambda = np.genfromtxt(spec_dir + gal_type + '.ascii').T
+            gal_SED = chroma.SED(g_wave, g_flambda)
+            for z in np.arange(0.0, 3.0, 0.03):
+                bar.update()
+                gal_SED.set_redshift(z)
+                data = compute_mags_moments(gal_SED, filters)
+                gal_data[i]['gal_type'] = gal_type
+                gal_data[i]['redshift'] = z
+                for name in data.dtype.names:
+                    gal_data[i][name] = data[name]
+                i += 1
+    np.save('galaxies.npy', gal_data)
+
+if __name__ == '__main__':
+    construct_analytic_table()
--- bin/reproduce/PB12/plot_relative_moments.py
+++ bin/reproduce/PB12/plot_relative_moments.py
@@ -7,66 +7,68 @@ import matplotlib.pyplot as plt
 import _mypath
 import chroma
 
-def compute_relative_moments(filter_name, zenith, **kwargs):
-    spec_dir = '../../../data/SEDs/'
-    filter_dir = '../../../data/filters/'
+# def compute_relative_moments(filter_name, zenith, **kwargs):
+#     spec_dir = '../../../data/SEDs/'
+#     filter_dir = '../../../data/filters/'
 
-    f_wave, f_throughput = np.genfromtxt(filter_dir + '{}.dat'.format(filter_name)).T
-    bandpass = chroma.Bandpass(f_wave, f_throughput)
-    # KIN galaxy spectra are cut-off above 994nm, so truncate redward wavelengths
-    # Note this makes y-band calculations impossible.
-    # Also truncate out-of-band leaks below 5e-3.
-    bandpass.truncate(rel_throughput=5.e-3, redlim=994)
+#     f_wave, f_throughput = np.genfromtxt(filter_dir + '{}.dat'.format(filter_name)).T
+#     bandpass = chroma.Bandpass(f_wave, f_throughput)
+#     # KIN galaxy spectra are cut-off above 994nm, so truncate redward wavelengths
+#     # Note this makes y-band calculations impossible.
+#     # Also truncate out-of-band leaks below 5e-3.
+#     bandpass.truncate(rel_throughput=5.e-3, redlim=994)
 
-    G5v_wave, G5v_flambda = np.genfromtxt(spec_dir + 'ukg5v.ascii').T
-    G5v_SED = chroma.SED(G5v_wave, G5v_flambda)
+#     G5v_wave, G5v_flambda = np.genfromtxt(spec_dir + 'ukg5v.ascii').T
+#     G5v_SED = chroma.SED(G5v_wave, G5v_flambda)
 
-    G5v_mom = G5v_SED.DCR_moment_shifts(bandpass, zenith, **kwargs)
+#     G5v_mom = G5v_SED.DCR_moment_shifts(bandpass, zenith, **kwargs)
 
-    star_types = ['uko5v',
-                  'ukb5iii',
-                  'uka5v',
-                  'ukf5v',
-                  'ukg5v',
-                  'ukk5v',
-                  'ukm5v']
-    star_diffs = {}
-    for star_type in star_types:
-        star_diffs[star_type] = {}
-        wave, flambda = np.genfromtxt(spec_dir + star_type + '.ascii').T
-        star_SED = chroma.SED(wave, flambda)
+#     star_types = ['uko5v',
+#                   'ukb5iii',
+#                   'uka5v',
+#                   'ukf5v',
+#                   'ukg5v',
+#                   'ukk5v',
+#                   'ukm5v']
+#     star_diffs = {}
+#     for star_type in star_types:
+#         star_diffs[star_type] = {}
+#         wave, flambda = np.genfromtxt(spec_dir + star_type + '.ascii').T
+#         star_SED = chroma.SED(wave, flambda)
 
-        m = star_SED.DCR_moment_shifts(bandpass, zenith, **kwargs)
-        star_diffs[star_type]['M1'] = (m[0] - G5v_mom[0]) * 180 / np.pi * 3600 # rad -> arcsec
-        # rad^2 -> arcsec^2
-        star_diffs[star_type]['M2'] = (m[1] - G5v_mom[1]) * (180 / np.pi * 3600)**2
+#         m = star_SED.DCR_moment_shifts(bandpass, zenith, **kwargs)
+#         star_diffs[star_type]['M1'] = (m[0] - G5v_mom[0]) * 180 / np.pi * 3600 # rad -> arcsec
+#         # rad^2 -> arcsec^2
+#         star_diffs[star_type]['M2'] = (m[1] - G5v_mom[1]) * (180 / np.pi * 3600)**2
 
-    gal_types= ['CWW_E_ext',
-                'KIN_Sa_ext',
-                'KIN_Sb_ext',
-                'CWW_Sbc_ext',
-                'CWW_Scd_ext',
-                'CWW_Im_ext',
-                'KIN_SB1_ext',
-                'KIN_SB6_ext']
+#     gal_types= ['CWW_E_ext',
+#                 'KIN_Sa_ext',
+#                 'KIN_Sb_ext',
+#                 'CWW_Sbc_ext',
+#                 'CWW_Scd_ext',
+#                 'CWW_Im_ext',
+#                 'KIN_SB1_ext',
+#                 'KIN_SB6_ext']
 
-    gal_diffs = {}
-    with astropy.utils.console.ProgressBar(100 * len(gal_types)) as bar:
-        for gal_type in gal_types:
-            gal_diffs[gal_type] = {'M1':[], 'M2':[], 'wM2':[]}
-            wave, flambda = np.genfromtxt(spec_dir + gal_type + '.ascii').T
-            gal_SED = chroma.SED(wave, flambda)
-            for z in np.arange(0.0, 1.3, 0.02):
-                bar.update()
-                gal_SED.set_redshift(z)
-                m = gal_SED.DCR_moment_shifts(bandpass, zenith, **kwargs)
-                # rad -> arcsec, rad^2 -> arcsec^2
-                gal_diffs[gal_type]['M1'].append((m[0] - G5v_mom[0]) * 180 / np.pi * 3600)
-                gal_diffs[gal_type]['M2'].append((m[1] - G5v_mom[1]) * (180 / np.pi * 3600)**2)
-    return star_diffs, gal_diffs
+#     gal_diffs = {}
+#     with astropy.utils.console.ProgressBar(100 * len(gal_types)) as bar:
+#         for gal_type in gal_types:
+#             gal_diffs[gal_type] = {'M1':[], 'M2':[], 'wM2':[]}
+#             wave, flambda = np.genfromtxt(spec_dir + gal_type + '.ascii').T
+#             gal_SED = chroma.SED(wave, flambda)
+#             for z in np.arange(0.0, 1.3, 0.02):
+#                 bar.update()
+#                 gal_SED.set_redshift(z)
+#                 m = gal_SED.DCR_moment_shifts(bandpass, zenith, **kwargs)
+#                 # rad -> arcsec, rad^2 -> arcsec^2
+#                 gal_diffs[gal_type]['M1'].append((m[0] - G5v_mom[0]) * 180 / np.pi * 3600)
+#                 gal_diffs[gal_type]['M2'].append((m[1] - G5v_mom[1]) * (180 / np.pi * 3600)**2)
+#     return star_diffs, gal_diffs
 
 def plot_analytic_moments(filter_name, zenith=45.0):
-    a_star_diff, a_gal_diff = compute_relative_moments(filter_name, zenith)
+    stars = np.load('../../analytic/stars.npy')
+    gals = np.load('../../analytic/galaxies.npy')
+
     #------------------------------#
     # Differences in first moments #
     #------------------------------#
@@ -81,33 +83,37 @@ def plot_analytic_moments(filter_name, zenith=45.0):
         os.mkdir('output/')
 
     star_names = ['O5v', 'B5iii', 'A5v', 'F5v', 'G5v', 'K5v', 'M5v']
-    stars = ['uko5v', 'ukb5iii', 'uka5v', 'ukf5v', 'ukg5v', 'ukk5v', 'ukm5v']
+    star_types = ['uko5v', 'ukb5iii', 'uka5v', 'ukf5v', 'ukg5v', 'ukk5v', 'ukm5v']
     star_colors = ['Blue', 'Cyan', 'Green', 'Gold', 'Orange', 'Red', 'Violet']
 
-    yvals1 = np.empty(0)
-    yvals2 = np.empty(0)
+    G_idx = stars['star_type'] == 'ukg5v'
+
     #plot stars
-    for star, star_name, star_color in zip(stars, star_names, star_colors):
-        analytic = a_star_diff[star]['M1']
-        ax1.scatter(0.0, analytic, c=star_color, marker='*', s=160, label=star_name)
+    for star_name, star_type, star_color in zip(star_names, star_types, star_colors):
+        star_idx = stars['star_type'] == star_type
+        Rbar = stars[star_idx]['Rbar'][filter_name]
+        dRbar = (Rbar - stars[G_idx]['Rbar'][filter_name]) * 180.0 / np.pi * 3600.0
+        ax1.scatter(0.0, dRbar, c=star_color, marker='*', s=160, label=star_name)
 
     gal_names = ['E', 'Sa', 'Sb', 'Sbc', 'Scd', 'Im', 'SB1', 'SB6']
-    gals = ['CWW_E_ext', 'KIN_Sa_ext', 'KIN_Sb_ext', 'CWW_Sbc_ext', 'CWW_Scd_ext',
-            'CWW_Im_ext', 'KIN_SB1_ext', 'KIN_SB6_ext']
+    gal_types = ['CWW_E_ext', 'KIN_Sa_ext', 'KIN_Sb_ext', 'CWW_Sbc_ext', 'CWW_Scd_ext',
+                 'CWW_Im_ext', 'KIN_SB1_ext', 'KIN_SB6_ext']
     gal_colors = ['Violet', 'Red', 'Orange', 'Gold', 'Green', 'Cyan', 'Blue']
 
-
     #plot gals
-    zs = np.arange(0.0, 1.3, 0.02)
-    for gal, gal_name, gal_color in zip(gals, gal_names, gal_colors):
-        analytic = a_gal_diff[gal]['M1']
-        ax1.plot(zs, analytic, c=gal_color, label=gal_name)
+    for gal_name, gal_type, gal_color in zip(gal_names, gal_types, gal_colors):
+        gal_idx = gals['gal_type'] == gal_type
+        Rbar = gals[gal_idx]['Rbar'][filter_name]
+        dRbar = (Rbar - stars[G_idx]['Rbar'][filter_name]) * 180.0 / np.pi * 3600.0
+        zs = gals[gal_idx]['redshift']
+        ax1.plot(zs, dRbar, c=gal_color, label=gal_name)
     ax1.legend(prop={"size":9})
 
     f.savefig('output/Rbar.{}.z{}.png'.format(filter_name, zenith))
 
-    # V plot, unweighted
-    ####################
+    #-------------------------------#
+    # Differences in second moments #
+    #-------------------------------#
 
     f = plt.figure(figsize=(8,6), dpi=100)
     ax1 = plt.subplot(111)
@@ -118,26 +124,20 @@ def plot_analytic_moments(filter_name, zenith=45.0):
     if not os.path.exists('output/'):
         os.mkdir('output/')
 
-    star_names = ['O5v', 'B5iii', 'A5v', 'F5v', 'G5v', 'K5v', 'M5v']
-    stars = ['uko5v', 'ukb5iii', 'uka5v', 'ukf5v', 'ukg5v', 'ukk5v', 'ukm5v']
-    star_colors = ['Blue', 'Cyan', 'Green', 'Gold', 'Orange', 'Red', 'Violet']
-
     #plot stars
-    for star, star_name, star_color in zip(stars, star_names, star_colors):
-        analytic = a_star_diff[star]['M2']
-        ax1.scatter(0.0, analytic, c=star_color, marker='*', s=160, label=star_name)
-
-    gal_names = ['E', 'Sa', 'Sb', 'Sbc', 'Scd', 'Im', 'SB1', 'SB6']
-    gals = ['CWW_E_ext', 'KIN_Sa_ext', 'KIN_Sb_ext', 'CWW_Sbc_ext', 'CWW_Scd_ext',
-            'CWW_Im_ext', 'KIN_SB1_ext', 'KIN_SB6_ext']
-    gal_colors = ['Violet', 'Red', 'Orange', 'Gold', 'Green', 'Cyan', 'Blue']
-
+    for star_name, star_type, star_color in zip(star_names, star_types, star_colors):
+        star_idx = stars['star_type'] == star_type
+        V = stars[star_idx]['V'][filter_name]
+        dV = (V - stars[G_idx]['V'][filter_name]) * (180.0 / np.pi * 3600.0)**2
+        ax1.scatter(0.0, dV, c=star_color, marker='*', s=160, label=star_name)
 
     #plot gals
-    zs = np.arange(0.0, 1.3, 0.02)
-    for gal, gal_name, gal_color in zip(gals, gal_names, gal_colors):
-        analytic = a_gal_diff[gal]['M2']
-        ax1.plot(zs, analytic, c=gal_color, label=gal_name)
+    for gal_name, gal_type, gal_color in zip(gal_names, gal_types, gal_colors):
+        gal_idx = gals['gal_type'] == gal_type
+        V = gals[gal_idx]['V'][filter_name]
+        dV = (V - stars[G_idx]['V'][filter_name]) * (180.0 / np.pi * 3600.0)**2
+        zs = gals[gal_idx]['redshift']
+        ax1.plot(zs, dV, c=gal_color, label=gal_name)
     ax1.legend(prop={"size":9})
 
     f.savefig('output/V.{}.z{}.png'.format(filter_name, zenith))
