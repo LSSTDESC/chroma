5222a6d9ef13954961f04ca1a6e2bee30d9ea264
jmeyers314 <jmeyers314@gmail.com>
2014-02-14 23:33:01 -0800

    replace simps with trapz

--- chroma/sed.py
+++ chroma/sed.py
@@ -1,9 +1,8 @@
 import numpy as np
-from scipy.integrate import simps
-import extinction
 
 import galsim
 
+import extinction
 import _mypath
 import chroma
 
@@ -23,7 +22,7 @@ class SED(galsim.SED):
 
     def magnitude(self, bandpass):
         wave_list = np.array(bandpass.wave_list)
-        flux = simps(bandpass(wave_list) * self(wave_list), wave_list)
+        flux = np.trapz(bandpass(wave_list) * self(wave_list), wave_list)
         return -2.5 * np.log10(flux) - bandpass.AB_zeropoint()
 
     def DCR_moment_shifts(self, bandpass, zenith, **kwargs):
@@ -32,9 +31,9 @@ class SED(galsim.SED):
         wave_list = np.array(bandpass.wave_list)
         R = chroma.atm_refrac(wave_list, zenith, **kwargs)
         photons = bandpass(wave_list) * self(wave_list)
-        norm = simps(photons, wave_list)
-        Rbar = simps(R * photons, wave_list) / norm
-        V = simps((R-Rbar)**2 * photons, wave_list) / norm
+        norm = np.trapz(photons, wave_list)
+        Rbar = np.trapz(R * photons, wave_list) / norm
+        V = np.trapz((R-Rbar)**2 * photons, wave_list) / norm
         return Rbar, V
 
     def seeing_shift(self, bandpass, alpha=-0.2, base_wavelength=500.0):
@@ -42,8 +41,8 @@ class SED(galsim.SED):
         """
         wave_list = np.array(bandpass.wave_list)
         photons = bandpass(wave_list) * self(wave_list)
-        return (simps(photons * (wave_list/500.0)**(2*alpha), wave_list) /
-                simps(photons, wave_list))
+        return (np.trapz(photons * (wave_list/500.0)**(2*alpha), wave_list) /
+                np.trapz(photons, wave_list))
 
 
 class Bandpass(galsim.Bandpass):
@@ -56,7 +55,7 @@ class Bandpass(galsim.Bandpass):
             # convert AB source from erg/s/Hz/cm^2*cm/s/nm^2 -> erg/s/cm^2/nm
             AB_flambda = AB_source * c / wave_list**2 / nm_to_cm
             AB_photons = AB_flambda * wave_list * self(wave_list)
-            AB_flux = simps(AB_photons, wave_list)
+            AB_flux = np.trapz(AB_photons, wave_list)
             self.zp = -2.5 * np.log10(AB_flux)
         return self.zp
 
