correct degrees/radians bug

bin/analytic/analytic_table.py
@@ -49,7 +49,7 @@ def compute_mags_moments(sed, filters):
                 out[0]['S_p10'][filter_name] = np.nan
         else:
             try:
-                DCR_mom = sed.DCR_moment_shifts(bandpass, zenith=45.0)
+                DCR_mom = sed.DCR_moment_shifts(bandpass, zenith=np.pi/4)
                 out[0]['Rbar'][filter_name] = DCR_mom[0]
                 out[0]['V'][filter_name] = DCR_mom[1]
                 out[0]['S_m02'][filter_name] = sed.seeing_shift(bandpass, alpha=-0.2)
@@ -101,7 +101,7 @@ def construct_analytic_table():
         star_data[i]['star_type'] = star_type
         for name in data.dtype.names:
             star_data[i][name] = data[name]
-    cPickle.dump(star_data, open('stars.pkl', 'wb'))
+    cPickle.dump(star_data, open('output/stars.pkl', 'wb'))
 
     # now onto galaxies
     gal_data = np.recarray((len(gal_types)*100,), dtype = [('gal_type', 'a11'),
@@ -125,7 +125,7 @@ def construct_analytic_table():
                 for name in data.dtype.names:
                     gal_data[i][name] = data[name]
                 i += 1
-    cPickle.dump(gal_data, open('galaxies.pkl', 'wb'))
+    cPickle.dump(gal_data, open('output/galaxies.pkl', 'wb'))
 
 if __name__ == '__main__':
     construct_analytic_table()