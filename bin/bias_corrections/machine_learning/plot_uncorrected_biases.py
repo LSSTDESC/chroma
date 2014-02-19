656973d4073e64221e016760b7d366d66edff95e
jmeyers314 <jmeyers314@gmail.com>
2014-02-19 00:13:44 -0800

    correct V0 bug

--- bin/bias_corrections/machine_learning/plot_uncorrected_biases.py
+++ bin/bias_corrections/machine_learning/plot_uncorrected_biases.py
@@ -11,6 +11,7 @@ hist_axes_range = [0.15, 0.12, 0.1, 0.8]
 scatter_axes_range = [0.25, 0.12, 0.55, 0.8]
 colorbar_axes_range = [0.85, 0.12, 0.04, 0.8]
 data_dir = '../../../data/'
+star_table = '../../analytic/stars.pkl'
 
 def hist_with_peak(x, bins=None, range=None, ax=None, orientation='vertical',
                    histtype=None, **kwargs):
@@ -47,18 +48,15 @@ def set_range(x):
     return [low - 0.3*span, high + 0.3*span]
 
 def R_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
-    filter_file = data_dir+'filters/{}.dat'.format(band)
-    star_SED_file = data_dir+'SEDs/ukg5v.ascii'
-    swave, sphotons = chroma.utils.get_photons(star_SED_file, filter_file, 0.0)
-    smom = chroma.disp_moments(swave, sphotons, zenith=np.pi/6)
-    R0 = smom[0] * 180/np.pi * 3600
+    table = cPickle.load(open(star_table))
+    R0 = table[table['star_type'] == 'ukg5v']['Rbar'][band][0]
 
     f = plt.figure(figsize=(8, 5))
 
     # scatter plot
     ax = f.add_axes(scatter_axes_range)
     x = gals.redshift
-    y = gals['R'][band] * np.tan(np.pi/6) * 180/np.pi * 3600 - R0
+    y = (gals['R'][band] - R0) * np.tan(np.pi/6) * 180/np.pi * 3600
     c = gals['magCalc'][cband1] - gals['magCalc'][cband2]
     clim = set_range(c)
     clim[1] += 0.1 * (clim[1]-clim[0])
@@ -82,7 +80,7 @@ def R_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
 
     # star histogram
     hist_ax = f.add_axes(hist_axes_range)
-    hist_with_peak(stars['R'][band] * np.tan(np.pi/6) * 180/np.pi * 3600 - R0, bins=200,
+    hist_with_peak((stars['R'][band] - R0) * np.tan(np.pi/6) * 180/np.pi * 3600, bins=200,
                    range=ylim, orientation='horizontal', histtype='stepfilled', color='blue')
     hist_ax.xaxis.set_ticklabels([])
     hist_ax.set_ylim(ylim)
@@ -90,7 +88,7 @@ def R_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
     hist_ax.set_xlim(xlim)
     hist_ax.fill_between(xlim, [ylim[0]]*2, [ylim[1]]*2, color='white', zorder=1)
     hist_ax.set_ylabel('$\Delta \overline{\mathrm{R}}$ (arcsec)', fontsize=12)
-    hist_with_peak(gals['R'][band] * np.tan(np.pi/6) * 180/np.pi * 3600 - R0, bins=200,
+    hist_with_peak((gals['R'][band] - R0) * np.tan(np.pi/6) * 180/np.pi * 3600, bins=200,
                    range=ylim, orientation='horizontal', histtype='step', color='red')
     hist_ax.text(xlim[0] + (xlim[1]-xlim[0])*0.2, ylim[1] - (ylim[1]-ylim[0])*0.08,
                  'stars', fontsize=12, color='blue')
@@ -110,18 +108,15 @@ def R_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
     f.savefig('output/dR_{}.png'.format(band), dpi=300)
 
 def V_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
-    filter_file = data_dir+'filters/{}.dat'.format(band)
-    star_SED_file = data_dir+'SEDs/ukg5v.ascii'
-    swave, sphotons = chroma.utils.get_photons(star_SED_file, filter_file, 0.0)
-    smom = chroma.disp_moments(swave, sphotons, zenith=np.pi/6)
-    V0 = smom[1] * (180/np.pi * 3600)**2
+    table = cPickle.load(open(star_table))
+    V0 = table[table['star_type'] == 'ukg5v']['V'][band][0]
 
     f = plt.figure(figsize=(8, 5))
 
     # scatter plot
     ax = f.add_axes(scatter_axes_range)
     x = gals.redshift
-    y = gals['V'][band] * (np.tan(np.pi/6) * 180/np.pi * 3600)**2 - V0
+    y = (gals['V'][band] - V0) * (np.tan(np.pi/6) * 180/np.pi * 3600)**2
     c = gals['magCalc'][cband1] - gals['magCalc'][cband2]
     clim = set_range(c)
     clim[1] += 0.1 * (clim[1]-clim[0])
@@ -145,7 +140,7 @@ def V_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
 
     # star histogram
     hist_ax = f.add_axes(hist_axes_range)
-    hist_with_peak(stars['V'][band] * (np.tan(np.pi/6) * 180/np.pi * 3600)**2 - V0, bins=200,
+    hist_with_peak((stars['V'][band] - V0) * (np.tan(np.pi/6) * 180/np.pi * 3600)**2, bins=200,
                    range=ylim, orientation='horizontal', histtype='stepfilled', color='blue')
     hist_ax.xaxis.set_ticklabels([])
     hist_ax.set_ylim(ylim)
@@ -153,7 +148,7 @@ def V_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
     hist_ax.set_xlim(xlim)
     hist_ax.fill_between(xlim, [ylim[0]]*2, [ylim[1]]*2, color='white', zorder=1)
     hist_ax.set_ylabel('$\Delta \mathrm{V}$ (arcsec$^2$)', fontsize=12)
-    hist_with_peak(gals['V'][band] * (np.tan(np.pi/6) * 180/np.pi * 3600)**2 - V0, bins=200,
+    hist_with_peak((gals['V'][band] - V0) * (np.tan(np.pi/6) * 180/np.pi * 3600)**2, bins=200,
                    range=ylim, orientation='horizontal', histtype='step', color='red')
     hist_ax.text(xlim[0] + (xlim[1]-xlim[0])*0.2, ylim[1] - (ylim[1]-ylim[0])*0.08,
                  'stars', fontsize=12, color='blue')
@@ -174,16 +169,6 @@ def V_vs_redshift(gals, stars, band, cband1, cband2, yrange=None, **kwargs):
     f.savefig('output/dV_{}.png'.format(band), dpi=300)
 
 def S_vs_redshift(gals, stars, band, cband1, cband2, alpha=-0.2, yrange=None, **kwargs):
-    filter_file = data_dir+'filters/{}.dat'.format(band)
-    star_SED_file = data_dir+'SEDs/ukg5v.ascii'
-    swave, sphotons = chroma.utils.get_photons(star_SED_file, filter_file, 0.0)
-    S0 = chroma.relative_second_moment_radius(swave, sphotons, alpha=alpha)
-
-    f = plt.figure(figsize=(8, 5))
-
-    # scatter plot
-    ax = f.add_axes(scatter_axes_range)
-    x = gals.redshift
     if alpha == -0.2:
         powidx = 'S_m02'
     elif alpha == 0.6:
@@ -192,6 +177,14 @@ def S_vs_redshift(gals, stars, band, cband1, cband2, alpha=-0.2, yrange=None, **
         powidx = 'S_p10'
     else:
         raise ValueError
+    table = cPickle.load(open(star_table))
+    S0 = table[table['star_type'] == 'ukg5v'][powidx][band][0]
+
+    f = plt.figure(figsize=(8, 5))
+
+    # scatter plot
+    ax = f.add_axes(scatter_axes_range)
+    x = gals.redshift
     y = np.log(gals[powidx][band] / S0 )
     c = gals['magCalc'][cband1] - gals['magCalc'][cband2]
     clim = set_range(c)
@@ -252,18 +245,18 @@ def S_vs_redshift(gals, stars, band, cband1, cband2, alpha=-0.2, yrange=None, **
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--galfile', default = 'corrected_galaxy_data.pkl',
-                        help="input galaxy file. Default 'corrected_galaxy_data.pkl'")
-    parser.add_argument('--starfile', default = 'corrected_star_data.pkl',
-                        help="input star file. Default 'corrected_star_data.pkl'")
+    parser.add_argument('--galfile', default = 'output/corrected_galaxy_data.pkl',
+                        help="input galaxy file. Default 'output/corrected_galaxy_data.pkl'")
+    parser.add_argument('--starfile', default = 'output/corrected_star_data.pkl',
+                        help="input star file. Default 'output/corrected_star_data.pkl'")
     args = parser.parse_args()
 
     gals = cPickle.load(open(args.galfile))
     stars = cPickle.load(open(args.starfile))
-    # R_vs_redshift(gals, stars, 'LSST_r', 'LSST_r', 'LSST_i', s=2, yrange=[-0.030, 0.015])
-    # V_vs_redshift(gals, stars, 'LSST_r', 'LSST_r', 'LSST_i', s=2, yrange=[-0.0008, 0.0005])
+    R_vs_redshift(gals, stars, 'LSST_r', 'LSST_r', 'LSST_i', s=2, yrange=[-0.030, 0.015])
+    V_vs_redshift(gals, stars, 'LSST_r', 'LSST_r', 'LSST_i', s=2, yrange=[-0.0008, 0.0005])
     S_vs_redshift(gals, stars, 'LSST_r', 'LSST_r', 'LSST_i', s=2, yrange=[-0.02, 0.01])
-    # R_vs_redshift(gals, stars, 'LSST_i', 'LSST_r', 'LSST_i', s=2, yrange=[-0.030, 0.015])
-    # V_vs_redshift(gals, stars, 'LSST_i', 'LSST_r', 'LSST_i', s=2, yrange=[-0.0008, 0.0005])
-    # S_vs_redshift(gals, stars, 'LSST_i', 'LSST_r', 'LSST_i', s=2, yrange=[-0.02, 0.01])
-    # S_vs_redshift(gals, stars, 'Euclid_350', 'LSST_r', 'LSST_i', alpha=0.6, s=2)
+    R_vs_redshift(gals, stars, 'LSST_i', 'LSST_r', 'LSST_i', s=2, yrange=[-0.030, 0.015])
+    V_vs_redshift(gals, stars, 'LSST_i', 'LSST_r', 'LSST_i', s=2, yrange=[-0.0008, 0.0005])
+    S_vs_redshift(gals, stars, 'LSST_i', 'LSST_r', 'LSST_i', s=2, yrange=[-0.02, 0.01])
+    S_vs_redshift(gals, stars, 'Euclid_350', 'LSST_r', 'LSST_i', alpha=0.6, s=2)
