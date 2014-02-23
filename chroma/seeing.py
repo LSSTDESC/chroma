9c3b4313eea0832dea13873712ede0ba61a7760a
jmeyers314 <jmeyers314@gmail.com>
2014-02-23 15:12:36 -0800

    Clean up some files

--- chroma/seeing.py
+++
@@ -1,7 +0,0 @@
-import numpy
-
-def relative_second_moment_radius(wave, photons, alpha=-0.2):
-    ''' Returns the second moment radius of PSF with specified SED, normalized at 500nm.
-    '''
-    return (numpy.trapz(photons * (wave/500) ** (2*alpha), wave) /
-            numpy.trapz(photons, wave))
