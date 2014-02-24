6d8270c2c2ec37092a0ccba2161fd60027f69f8f
jmeyers314 <jmeyers314@gmail.com>
2014-02-23 16:57:30 -0800

    update comments for sed

--- chroma/sed.py
+++ chroma/sed.py
@@ -5,12 +5,19 @@ import galsim
 import dcr
 
 class SED(galsim.SED):
+    """ Subclass galsim.SED to add a few additional methods for calculating magnitudes and
+    chromatic biases.
+    """
     def createWithMagnitude(self, bandpass, target_magnitude):
+        """ Return a new SED with AB Magnitude through `bandpass` set to `target_magnitude`.
+        """
         current_mag = self.getMagnitude(bandpass)
         scale = 10**(-0.4 * (target_magnitude - current_mag))
         return self * scale
 
     def getMagnitude(self, bandpass):
+        """ Return AB magnitude of SED measured through `bandpass`.
+        """
         wave_list = np.array(bandpass.wave_list)
         flux = np.trapz(bandpass(wave_list) * self(wave_list), wave_list)
         return -2.5 * np.log10(flux) - bandpass.getABZeropoint()
@@ -26,8 +33,8 @@ class SED(galsim.SED):
         V = np.trapz((R-Rbar)**2 * photons, wave_list) / norm
         return Rbar, V
 
-    def getSeeingShift(self, bandpass, alpha=-0.2, base_wavelength=500.0):
-        """ Calculates relative size of PSF that scales like a powerlaw in wavelength.
+    def getSeeingShift(self, bandpass, alpha=-0.2):
+        """ Calculates relative size of PSF compared to PSF size at 500 nm.
         """
         wave_list = np.array(bandpass.wave_list)
         photons = bandpass(wave_list) * self(wave_list)
@@ -36,6 +43,8 @@ class SED(galsim.SED):
 
 
 class Bandpass(galsim.Bandpass):
+    """ Subclass galsim.Bandpass to add zeropoints
+    """
     def getABZeropoint(self):
         if not (hasattr(self, 'zp')):
             AB_source = 3631e-23 # 3631 Jy -> erg/s/Hz/cm^2
@@ -48,6 +57,11 @@ class Bandpass(galsim.Bandpass):
             AB_flux = np.trapz(AB_photons, wave_list)
             self.zp = -2.5 * np.log10(AB_flux)
         return self.zp
+
+    def createTruncated(self, relative_throughput=None, blue_limit=None, red_limit=None):
+        ret = galsim.Bandpass.createTruncated(self, relative_throughput, blue_limit, red_limit)
+        ret.__class__ = Bandpass
+        return ret
 
     def createThinned(self, step):
         ret = galsim.Bandpass.createThinned(self, step)
