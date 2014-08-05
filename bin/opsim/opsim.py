fix bug with \Delta RA computation

bin/opsim/opsim.py
@@ -470,20 +470,20 @@ def epoch_variance_field(cat, field):
     fig = plt.figure(figsize=(7, 5))
     ax = fig.add_subplot(111)
     ax.scatter(x, y, s=5)
-    ax.scatter(centroid[0], centroid[1], marker='x', color='red')
+    ax.scatter(centroid[0], centroid[1], marker='x', color='red', s=80)
     ax.set_xlabel(r"$\cos\delta\, \Delta \alpha / \Delta \bar{R}_{45}$")
     ax.set_ylabel(r"$\Delta \delta / \Delta \bar{R}_{45}$")
     ax.text(0.5, 0.9, r"$\delta$ = {:5.3f}$^\circ$".format(cat[w]['fieldDec'][0] * 180/np.pi),
             transform=ax.transAxes)
     ax.text(0.5, 0.84, r"$\alpha$ = {:5.3f}$^\circ$".format(cat[w]['fieldRA'][0] * 180/np.pi),
             transform=ax.transAxes)
-    ax.text(0.5, 0.78, r"$\langle(\cos \delta\, \Delta\alpha)^2\rangle/\bar{R}_{45}^2$ = "+
+    ax.text(0.5, 0.78, r"$\langle(\cos \delta\, \Delta\alpha)^2\rangle/(\Delta \bar{R}_{45})^2$ = "+
                        "{:5.3f}".format(dx),
                        transform=ax.transAxes)
-    ax.text(0.5, 0.72, r"$\langle(\Delta\delta)^2\rangle/\bar{R}_{45}^2$ = "+
+    ax.text(0.5, 0.72, r"$\langle(\Delta\delta)^2\rangle/(\Delta \bar{R}_{45})^2$ = "+
                        "{:5.3f}".format(dy),
                        transform=ax.transAxes)
-    ax.text(0.5, 0.66, r"$\langle(\cos \delta\, \Delta\alpha) (\Delta \delta)\rangle/\bar{R}_{45}^2$ = "+
+    ax.text(0.5, 0.66, r"$\langle(\cos \delta\, \Delta\alpha) (\Delta \delta)\rangle/(\Delta\bar{R}_{45})^2$ = "+
                        "{:5.3f}".format(dxy),
                        transform=ax.transAxes)
     fig.tight_layout()