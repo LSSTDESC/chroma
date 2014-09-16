8339bd21e0ad84b0557018c37535693e8542b021
jmeyers314 <jmeyers314@gmail.com>
2014-09-16 15:17:13 -0700

    refactor chromatic bias requirements table generator

--- bin/analytic/tables/table3.py
+++ bin/analytic/tables/table3.py
@@ -1,41 +1,54 @@
+import numpy as np
+
 # order is DES, LSST
-r2galsqr = [0.6**2, 0.4**2]
-r2PSFsqr = [0.8**2, 0.7**2]
+r2sqr_gal = np.r_[0.6, 0.4]**2
+r2sqr_PSF = np.r_[0.8, 0.7]**2
 
-m_req = [0.008, 0.003]
-cRMS_req = [0.0025, 0.0015]
-
+mean_m_req = np.r_[0.008, 0.003]
 shear_var = 4.e-4
+var_c_req = mean_m_req * 2 * shear_var
+print var_c_req
 
-def MeanDeltaV(m, r2galsqr):
-    return m * r2galsqr
+psf_ellip = 0.05
 
-def RMSDeltaV(c, r2galsqr):
-    return c * r2galsqr * 2
+mean_DeltaRbarSqr_req = mean_m_req * 1.0
+var_DeltaRbarSqr_req = var_c_req * 0.5**2
 
-def MeanDeltaRbar(m, r2galsqr):
+mean_DeltaV_req = r2sqr_gal * mean_m_req
+var_DeltaV_req = r2sqr_gal**2 * var_c_req
 
+mean_dr_by_dr_req = mean_m_req * r2sqr_gal / r2sqr_PSF
+var_dr_by_dr_req = var_c_req * (r2sqr_gal / r2sqr_PSF * 2.0 / psf_ellip)**2
 
-
+DESreqs = [a[0] for a in [mean_DeltaRbarSqr_req,
+                          var_DeltaRbarSqr_req,
+                          mean_DeltaV_req,
+                          var_DeltaV_req,
+                          mean_dr_by_dr_req,
+                          var_dr_by_dr_req]]
+LSSTreqs = [a[1] for a in [mean_DeltaRbarSqr_req,
+                           var_DeltaRbarSqr_req,
+                           mean_DeltaV_req,
+                           var_DeltaV_req,
+                           mean_dr_by_dr_req,
+                           var_dr_by_dr_req]]
 
 outfile = open("chromatic_requirements.tex", 'w')
 outfile.write("\\begin{deluxetable*}{lcccccc}\n")
 outfile.write("  \\tablecaption{\label{table:chromatic_requirements} Requirements on chromatic bias parameters. }\n")
 outfile.write("  \\tablehead{\n")
 outfile.write("    \colhead{Survey} &\n")
-outfile.write("    \colhead{$|\langle\Delta \\bar{R}\\rangle|$} &\n")
-outfile.write("    \colhead{$(\Delta \\bar{R})_\mathrm{RMS}$} &\n")
-outfile.write("    \colhead{$|\langle\Delta V\\rangle|$} &\n")
-outfile.write("    \colhead{$(\Delta V)_\mathrm{RMS}$} &\n")
-outfile.write("    \colhead{$|\langle\Delta r^2_\mathrm{psf}/r^2_\mathrm{psf}\\rangle|$} &\n")
-outfile.write("    \colhead{$(\Delta r^2_\mathrm{psf}/r^2_\mathrm{psf})_\mathrm{RMS}$}\n")
+outfile.write("    \colhead{$\langle(\Delta \\bar{R}_{45})^2\\rangle_\mathrm{SEDs}$} &\n")
+outfile.write("    \colhead{$\mathrm{Var}((\Delta \\bar{R}_{45})^2)_\mathrm{SEDs}$} &\n")
+outfile.write("    \colhead{$|\langle\Delta V\\rangle_\mathrm{SEDs}|$} &\n")
+outfile.write("    \colhead{$\mathrm{Var}(\Delta V)_\mathrm{SEDs}$} &\n")
+outfile.write("    \colhead{$|\langle\Delta r^2_\mathrm{psf}/r^2_\mathrm{psf}\\rangle_\mathrm{SEDs}|$} &\n")
+outfile.write("    \colhead{$\mathrm{Var}(\Delta r^2_\mathrm{psf}/r^2_\mathrm{psf})_\mathrm{SEDs}$}\n")
 outfile.write("  }\n")
 outfile.write("  \startdata\n")
-DESreqs = []
-outfile.write("    DES  & {:5.3f} & {:5.3f} & {:5.3f} & {:6.4f} & {:5.3f} & {5.3f}")
-outfile.write("    DES  & 0.045 & 0.036 & 0.002  & 0.0013 & 0.003  & 0.065 \\\n")
-outfile.write("    LSST & 0.02  & 0.02  & 0.0004 & 0.0004 & 0.0008 & 0.016\n")
+outfile.write("    DES  & ${:.4e}$ & ${:.4e}$ & ${:.4e}$ & ${:.4e}$ & ${:.4e}$ & ${:.4e}$ \\\\\n".format(*DESreqs))
+outfile.write("    LSST & ${:.4e}$ & ${:.4e}$ & ${:.4e}$ & ${:.4e}$ & ${:.4e}$ & ${:.4e}$\n".format(*LSSTreqs))
 outfile.write("  \enddata\n")
 outfile.write("  \\tablecomments{Requirements on bias parameters for differential chromatic refraction ($\Delta \\bar{R}$ and $\Delta V$) and for chromatic seeing ($\Delta r^2_\mathrm{psf}/r^2_\mathrm{psf}$), defined in Equations~\\ref{eqn:Rbar}, \\ref{eqn:V}, and \\ref{eqn:delta_r2}.  Units are in arcseconds for $\Delta R$ and square arcseconds for $\Delta V$.\n")
 outfile.write("  These are the values for which each bias by itself would degrade the accuracy and/or precision of the survey by an amount equivalent to the statistical sensitivity.}\n")
-outfile.write("\end{deluxetable*}\n")
+outfile.write("\\end{deluxetable*}\n")
