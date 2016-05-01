4717e8bbb1dad59c021f0d7738066cc614ed9953
Josh Meyers <jmeyers314@gmail.com>
2016-05-01 14:32:59 -0700

    More options for chromatic_biases plot.

--- bin/intuition/refraction.py
+++ bin/intuition/refraction.py
@@ -32,16 +32,22 @@ def refraction_vs_zenith():
         os.mkdir('output/')
     fig.savefig('output/refraction_vs_zenith.png', dpi=220)
 
-def chromatic_biases():
+def chromatic_biases(Euclid=True, chromatic_seeing=True, wide=True):
     zeniths = [10, 20, 30, 40, 50]
     waves = np.arange(300, 1101, 1, dtype=np.float64)
 
-    fig = plt.figure(figsize=(8,5))
+    if wide:
+        fig = plt.figure(figsize=(8, 3.5))
+        ax = fig.add_axes([0.15, 0.16, 0.74, 0.74])
+        fontsize=15
+    else:
+        fig = plt.figure(figsize=(8,5))
+        ax = fig.add_axes([0.14, 0.13, 0.76, 0.78])
+        fontsize=18
     #ax = fig.add_subplot(111)
-    ax = fig.add_axes([0.14, 0.13, 0.76, 0.78])
-    ax.set_xlabel('Wavelength (nm)', fontsize=18)
-    ax.set_xlim(300, 1200)
-    ax.set_ylabel('Relative refraction (arcsec)', fontsize=18)
+    ax.set_xlabel('Wavelength (nm)', fontsize=fontsize)
+    ax.set_xlim(300, 1100)
+    ax.set_ylabel('Relative refraction (arcsec)', fontsize=fontsize)
     ax.set_ylim(-1, 1.)
 
     for zenith in zeniths:
@@ -51,9 +57,10 @@ def chromatic_biases():
         # chroma output is also in radians, so convert to arcsec here
         ax.plot(waves, (refrac_angle - refrac_ref) * 206265, label=str(zenith)+' deg')
 
-    # 350nm wide Euclid filter.
-    ax.fill_between([0., 550., 550., 900., 900., 1200.], [-1, -1, 0.25, 0.25, -1, -1], -1,
-                    color='black', alpha=0.15)
+    if Euclid:
+        # 350nm wide Euclid filter.
+        ax.fill_between([0., 550., 550., 900., 900., 1200.], [-1, -1, 0.25, 0.25, -1, -1], -1,
+                        color='black', alpha=0.15)
 
     colors = ['purple', 'blue', 'green', 'gold', 'magenta', 'red']
     for i, filter_ in enumerate('ugrizy'):
@@ -61,25 +68,31 @@ def chromatic_biases():
         fdata = chroma.Bandpass(datadir+'filters/LSST_{}.dat'.format(filter_))
         fwave, throughput = fdata.wave_list, fdata(fdata.wave_list)
         ax.fill_between(fwave, throughput * 2.5 - 1, -1, color=colors[i], alpha=0.3)
-    # Add in lambda^(-2/5) for chromatic seeing comparison integrand comparison
-    ax2 = ax.twinx()
-    ys = (waves/500.0)**(-2./5)
-    ax2.plot(waves, ys, 'k', lw=3, label='$\lambda^{-2/5}$')
     ax.legend(fontsize=11, title='zenith angle')
-    ax2.legend(fontsize=11, title='chromatic seeing', loc='upper right', bbox_to_anchor = (0.78, 1))
-    ax2.set_xlim(300, 1100)
-    ax2.set_ylabel('Relative $r^2_\mathrm{PSF}$', fontsize=18)
+    if chromatic_seeing:
+        # Add in lambda^(-2/5) for chromatic seeing comparison integrand comparison
+        ax2 = ax.twinx()
+        ys = (waves/500.0)**(-2./5)
+        ax2.plot(waves, ys, 'k', lw=3, label='$\lambda^{-2/5}$')
+        ax2.legend(fontsize=11, title='chromatic seeing', loc='upper right', bbox_to_anchor = (0.78, 1))
+        ax2.set_xlim(300, 1100)
+        ax2.set_ylabel('Relative $r^2_\mathrm{PSF}$', fontsize=fontsize)
+        for label in ax2.get_yticklabels():
+            label.set_fontsize(fontsize)
 
     for label in ax.get_xticklabels():
-        label.set_fontsize(18)
+        label.set_fontsize(fontsize)
     for label in ax.get_yticklabels():
-        label.set_fontsize(18)
-    for label in ax2.get_yticklabels():
-        label.set_fontsize(18)
+        label.set_fontsize(fontsize)
 
     if not os.path.isdir('output/'):
         os.mkdir('output/')
-    fig.savefig('output/chromatic_biases.png', dpi=220)
+    fn = 'chromatic_biases'
+    if Euclid:
+        fn += '.Euclid'
+    if chromatic_seeing:
+        fn += '.chromatic_seeing'
+    fig.savefig('output/'+fn+'.png', dpi=220)
 
 def chromatic_biases_700():
     zeniths = [10, 20, 30, 40, 50]
@@ -141,5 +154,6 @@ def chromatic_biases_700():
 
 if __name__ == '__main__':
     refraction_vs_zenith()
-    chromatic_biases()
+    chromatic_biases(wide=True)
+    chromatic_biases(Euclid=False, chromatic_seeing=False, wide=True)
     chromatic_biases_700()
